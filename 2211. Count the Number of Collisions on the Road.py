d54d9ee2450d91c7533fdb914bc00e8002371fdf
cyyc889 <mb703450@gmail.com>
2025-12-04 14:30:44 +0800

    -

--- 2211. Count the Number of Collisions on the Road.py
+++ 2211. Count the Number of Collisions on the Road.py
@@ -1,21 +1,20 @@
+# 解题的核心思想是flag存储前一辆车的方向，判断flag的变化条件容易出错
 class Solution:
     def countCollisions(self, directions: str) -> int:
         res = 0
-        flag = -1  # 标记碰撞区间，-1代表没有碰撞
-
+        flag = -1
         for c in directions:
-            if c == 'L':  # 左行车
-                if flag >= 0:  # 碰撞区间存在
-                    res += flag + 1  # 增加碰撞次数
-                    flag = 0  # 碰撞后该车停止
-            elif c == 'S':  # 静止车
-                if flag > 0:  # 有车正在行驶
-                    res += flag  # 增加碰撞次数
-                flag = 0  # 静止车停止
-            else:  # 右行车
-                if flag >= 0:  # 继续增加碰撞区间长度
+            if c == 'L':
+                if flag >= 0:
+                    res += flag + 1
+                    flag = 0
+            elif c == 'S':
+                if flag >= 0:
+                    res += flag
+                flag = 0
+            else:
+                if flag >= 0:
                     flag += 1
                 else:
-                    flag = 1  # 启动碰撞区间
-
+                    flag = 1
         return res
